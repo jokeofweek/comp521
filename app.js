8013c926ad4c12ac121a4e2574924f2b6ceeb228
Alex Coco <alex.coco@shopify.com>
2015-04-07 14:20:25 -0400

    Use renderer

--- app.js
+++ app.js
@@ -1,48 +1,43 @@
 // Draw the noise values
 var octaves = 7;
-var waterLevel = 0.3;
-var seed = Math.random() * 200;
-var width = 500;
-var halfWidth = width / 2;
-var height = 500;
-var halfHeight = height / 2;
+var size = 500;
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+// Seed the RNG
+noise.seed(Math.random());
 
-canvas.height = height;
-canvas.width = width;
+var renderer = new Renderer(document.getElementById('canvas'), size);
 
-var maxDistance = 0.8 * Math.sqrt(halfWidth * halfWidth + halfHeight * halfHeight);
+var maxDistance = 0.8 * Math.sqrt(size*size/2);
+var heightmap = new Uint8ClampedArray(size*size);
 
-for (var y = 0; y < height; y++) {
-  for (var x = 0; x < width; x++) {
-    var value = 0;
-    for (var o = 1 << octaves; o >= 1; o >>= 1) {
-      value += noise.perlin2((x + seed) / o, (y + seed) / o) * o;
-    }
+for (var x = 0; x < size; x++) {
+  for (var y = 0; y < size; y++) {
 
-    // Get the average value
-    value /= (2 << octaves);
+    // https://github.com/Hypercubed/perlin-terrain-angular-demo/blob/bd6df90b055a555194dd4aa09cbe9776de2de7f0/script.js#L38-L51
+    value = (function(x,y,N) {
+      var z = 0, s = 0;
+      for (var i = 0; i < N; i++) {
+        var pp = 1/(1 << i);
+        var e = Math.PI*pp/2;
+        var ss = Math.sin(e);
+        var cc = Math.cos(e);
+        var xx = (x*ss+y*cc);
+        var yy = (-x*cc+y*ss);
+        s += pp;
+        z += pp*Math.abs(noise.perlin2(xx/pp,yy/pp));
+      }
+      return 2*z/s;
+    })(x/size, y/size, octaves);
 
-    // Normalize to [0, 1]
-    value = (value + 1) / 2;
-
-    // Ensure an island/circular shape.
-    var xDist = x - halfWidth;
-    var yDist = y - halfHeight;
+    // // Ensure an island/circular shape.
+    var xDist = x - size/2;
+    var yDist = y - size/2;
     var dist = Math.sqrt(xDist * xDist + yDist * yDist);
     
     value *= Math.max(0, 1 - Math.pow(dist / maxDistance, 2));
 
-    var component = Math.floor(value * 255);
-
-    if (value < waterLevel) {
-      ctx.fillStyle = 'rgb(0, 0, ' + (30 + component) + ')'
-    } else {
-      ctx.fillStyle = 'rgb(' + component + ',' + component + ',' + component + ')';
-    }
-    ctx.fillRect(x, y, 1, 1);
+    heightmap[y * size + x] = 255 * value;
   }
 }
 
+renderer.render(heightmap);
